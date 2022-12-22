e9cbbfd508ab93c87349e9c75c439c6da21d3854
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2022-12-22 18:29:13 +0530

    Update process.c

--- process.c
+++ process.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <dirent.h>
-#include <string.h>
 
-int main() {
-  DIR *dir = NULL;
-  struct dirent *ptr = NULL;
-  FILE *fp = NULL;
-  char filepath[256];
-  char filetext[128];
-  dir = opendir("/proc");
-  if (NULL != dir) {
-    while ((ptr = readdir(dir)) != NULL) {
-  	  if ((strcmp(ptr->d_name, ".") == 0) || (strcmp(ptr->d_name, "..") == 0)) {
-  			continue;
-  		}
-  		if (ptr->d_type != DT_DIR) {
-  		  continue;
-  		}
-  		sprintf(filepath, "/proc/%s/cmdline", ptr->d_name);
-  		fp = fopen(filepath, "r");
-  		if (NULL != fp) {
-  		  fgets(filetext, sizeof(filetext), fp);
-  		  printf("Process Name: '%s' Process Id: '%d'\n", filetext, atoi(ptr->d_name));
-  			fclose(fp);
-  		}
-  	}
-  }
-  closedir(dir);
-  return 0;
+int main(int argc, char **argv) {
+	int id, fd;
+	DIR *dir;
+	char data[128];
+	struct dirent* entry;
+	
+	dir = opendir("/proc");
+	
+	if (dir == NULL) {
+		printf("Failed to open proc\n");
+		exit(1);
+	}
+	
+	while ((entry = readdir(dir)) != NULL) {
+		sscanf(entry->d_name,"%d",&id);
+		if (id != 0) {
+			sprintf(data, "/proc/%d/cmdline", id);
+			fd = open(data, O_RDONLY);
+            if (fd != -1) {
+            	read(fd, data, 127);
+                printf("[%d] %s\n", id, data);
+            }
+		}
+	}
+	
+	closedir(dir);
+	exit(0);
 }
