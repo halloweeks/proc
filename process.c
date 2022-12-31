96e6e2e2bc5f0d2f4554a1946912db56af0360e7
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2022-12-31 17:38:12 +0530

    Update process.c

--- process.c
+++ process.c
@@ -7,22 +7,22 @@
 int main() {
 	int id, fd;
 	DIR *dir;
-	char data[128];
+	char data[128] = {0};
 	struct dirent* entry;
 	
-	if ((dir = opendir("/proc")) != NULL) {
+	if ((dir = opendir("/proc")) == NULL) {
 		printf("Failed to open proc\n");
 		exit(EXIT_FAILURE);
 	}
 	
 	while ((entry = readdir(dir)) != NULL) {
-		sscanf(entry->d_name,"%d",&id);
+		sscanf(entry->d_name, "%d", &id);
 		if (id != 0) {
-			sprintf(data, "/proc/%d/cmdline", id);
+			snprintf(data, 127, "/proc/%d/cmdline", id);
 			if ((fd = open(data, O_RDONLY)) != -1) {
-            	read(fd, data, 127);
-                printf("[%d] %s\n", id, data);
-                close(fd);
+				read(fd, data, 127);
+				printf("[%d] %s\n", id, data);
+				close(fd);
             }
 		}
 	}
